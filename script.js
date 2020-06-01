9e12c5a17cb65748a7e1f049bbb226248d25995c
Laura Weyrick <65510690+L2Fly@users.noreply.github.com>
2020-05-31 23:55:40 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var doAction = function () {
+    var input = document.getElementById('numberinput');
+    var number =parseInt(input.value);
+
+    for(var i = 0; i < 5; i++) {
+      document.write(number)
+    }
+}
